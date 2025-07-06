278273c14448cb9d29b4dd40522c40f448b822e5
Dean Mark <deansy0718@gmail.com>
2025-07-06 21:34:00 +0800

    done, checkbox function next

--- index.js
+++ index.js
@@ -1,27 +1,60 @@
-const myArray = [];
+const mainArray = [{
+                name: 'Make dinner',
+                date: '12:06'
+            },
+            {
+                name: 'Wash dishes',
+                date: '08:44'
+            }
+];
 
-function renderTodo() {
-    const main = document.querySelector('.main');
-    let html = '';
-    for (let i = 0; i < myArray.length; i++) {
-        const getValue = myArray[i];
-        const getHtml = `<p>${getValue} 
-        <button 
-            onclick="myArray.splice(${i}, 1)
-            renderTodo()">Delete
-        </button>
-        </p> 
-        `;
-        html += getHtml;
-    }
-main.innerHTML = html;
-}
-
+const container = document.querySelector('.container');
+addFunction()
 function addFunction() {
-    const text1 = document.querySelector('.input-text');
-    const getText1 = text1.value;
-    
-    myArray.push(getText1)
-    renderTodo();
-    text1.value = '';
+    let html = '';
+    mainArray.forEach(function (value, i) {
+        let getValue = `
+        <div class="element"><input type="checkbox" onchange="checkFunction(this)" class="js-check"></div>
+        <div class="element line">${value.name}</div>
+        <div class="element line1">${value.date}</div>
+        <button class="btnDelete element" onclick="mainArray.splice(${i}, 1); addFunction()">Delete</button>
+        `
+        html += getValue;
+    });
+container.innerHTML = html;
+return html;
+};
+
+function addInput() {
+    const name = document.querySelector('.js-input');
+    const getName = name.value;
+    const date = document.querySelector('.js-date');
+    const getDate = date.value;
+
+    if (!getName || !getDate) {
+        alert('Please enter the empty field.');
+    } else {
+         mainArray.push({name: getName, date: getDate})
+        addFunction()
+        name.value = ''; 
+        date.value = '';
+    };  
+};
+
+function checkFunction(checkbox) {
+    const line = document.querySelector('.line');
+    const line1 = document.querySelector('.line1');
+
+    if (checkbox.checked) {
+        line.style.textDecoration = 'line-through';
+        line1.style.textDecoration = 'line-through';
+        line.style.opacity = '0.6';
+        line1.style.opacity = '0.6';
+    }
+    else {
+        line.style.textDecoration = 'none';
+        line1.style.textDecoration = 'none';
+        line.style.opacity = '1';
+        line1.style.opacity = '1';
+    }
 }
